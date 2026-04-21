6a92260d8bfd5a115aaabfcf2e2276f44c141885
edurivas2020-afk <edu.rivas2020@gmail.com>
2026-04-21 10:52:52 -0400

    subiendo conversor

---
+++ conversor_temperatura.py
@@ -0,0 +1,10 @@
+# teniendo 3 escalas de temperatura (celsius,fahrenheit y kelvin)
+# cree un conversor de temperaturas que le pida al usuario la temperatura y escala inicial
+# y la escala a la que desea convertir, lurgo muestre el resultado de la coversion 
+
+# °c a °f = 1,8°c + 32°
+# f a °c = 5/9(°f - 32°)
+
+# °c a k = °c +273°
+# k a °c = k - 273°
+
